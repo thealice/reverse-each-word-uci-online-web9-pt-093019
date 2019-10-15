7bcbd64c2fb5ed55c3b3002ed6417616a632198c
Alice Freda <alicefreda@gmail.com>
2019-10-15 20:59:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   array = string.split
-  reversed = array.reverse_each { |words| print "#{words}"}
+  reversed = array.sort { |words| print "#{words}"}
   reversed
 end
